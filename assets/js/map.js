6938301ba1076c0aa7ebadc1d7b22cca384510e4
Anonymous <44589447+anonymous-tld@users.noreply.github.com>
2020-10-09 11:42:29 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -21,13 +21,13 @@ var maskIcon = L.icon({
 var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
 }).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
-controlLayers.addBaseLayer(light, 'Carto Light basemap');
+controlLayers.addBaseLayer(light, 'Einfache Karte');
 
 /* Stamen colored terrain basemap tiles with labels */
 var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
-controlLayers.addBaseLayer(terrain, 'Stamen Terrain basemap');
+controlLayers.addBaseLayer(terrain, 'Erweiterte Karte');
 
 // see more basemap options at https://leaflet-extras.github.io/leaflet-providers/preview/
 geocoder = new L.Control.Geocoder.Nominatim();
